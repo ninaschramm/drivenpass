69630e1a81a0b05cfd7774d0a4ad70ffb3c337e9
ninaschramm <schrammnina@outlook.com>
2022-09-07 15:57:19 -0300

    initial server config

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import express, { json, NextFunction } from 'express';
+import cors from 'cors';
+
+// import router from './routes/index.js';
+
+const server = express();
+
+dotenv.config();
+
+server.use(cors())
+server.use(json())
+
+
+// server.use(router);
+
+
+const PORT: number = +process.env.PORT || 5000
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+});
